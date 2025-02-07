b9ce1ec4656a721623a79f4f271fa94e2bded53c
guidias2002 <guidias200220@gmail.com>
2025-02-07 11:28:43 -0300

    feat: first commit

---
+++ src/main/java/com/microservico/envia/email/Application.java
@@ -0,0 +1,13 @@
+package com.microservico.envia.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
